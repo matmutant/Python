c2a20e21c46c85bfbfe051014a41385c53c3e115
matmutant <matmutant@gmail.com>
2014-11-04 11:46:45 +0100

    Year.py is now shortened using mixed conditions

--- Year/Year.py
+++ Year/Year.py
@@ -2,19 +2,7 @@
 year = input("type a year : ")
 year = int(year) #convert variable from string to integer
 print(year)
-leap = False
-if year % 4 == 0:
-	if year % 100 == 0:
-		if year % 400 == 0:
-			leap=True
-		else:
-			leap = False
-	else:
-		leap = True
-else:
-	leap = False
-
-if leap:
+if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
 	print("Leap Year")
 else:
 	print("nope")

b1488e6b04a65094ba3ccea28add57e881e568cd
matmutant <matmutant@gmail.com>
2014-11-04 11:31:00 +0100

    initial commit for Year.py

---
+++ Year/Year.py
@@ -0,0 +1,9 @@
+# -*-coding:Utf-8 -*
+annee = input("saisissez une années : ")
+annee = int(annee) #convert variable from string to integer
+print(annee)
+
+
+
+
+input("Appuyez sur ENTREE pour fermer ce programme...")

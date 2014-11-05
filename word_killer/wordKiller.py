db7584f612a3514d296354a7bd50348fb4ecc879
matmutant <matmutant@gmail.com>
2014-11-05 18:27:06 +0100

    added an end when word is destroyed

--- word_killer/wordKiller.py
+++ word_killer/wordKiller.py
@@ -1,14 +1,19 @@
 # -*-coding:Utf-8 -*
 wordToKill = input("enter word to kill: ")
-
 Rampage = True
 while Rampage:
 	letterToKill = input("enter letter to remove: ")
 	for i in wordToKill:
 		if i == letterToKill:
 			wordToKill = wordToKill.split(i)
-			wordToKill = "_".join(wordToKill)
+			wordToKill = "".join(wordToKill)
 	print(wordToKill)
+	if wordToKill:
+		print("letters left")
+	else:
+		print("Word nuked")
+		Rampage = False
+		continue
 	continueGame = input("continue? (y/n)")
 	if continueGame.lower() == "n":
 		Rampage = False
